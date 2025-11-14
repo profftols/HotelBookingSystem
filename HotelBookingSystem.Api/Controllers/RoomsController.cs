507041e8fc0bb8f0f0ae41cd5f0d50997a134d3d
Korikov Ivan <i.korikov.91@gmail.com>
2025-11-14 17:36:04 +0500

    job

--- HotelBookingSystem.Api/Controllers/RoomsController.cs
+++ HotelBookingSystem.Api/Controllers/RoomsController.cs
@@ -7,17 +7,27 @@ namespace HotelBookingSystem.Api.Controllers;
 [Route("api/[controller]")]
 public class RoomsController : ControllerBase
 {
+    private static readonly List<Room> _rooms = new();
+    private static int _next = 1;
+    
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Room>> GetRoomById(int id)
     {
-        var room = new Room(id, $"Room {id}", RoomStatus.Available);
-        return Ok(room);
+        var room = _rooms.FirstOrDefault(r => r.Id == id);
+        return room is null ? NotFound() : Ok(room);
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Room>>> GetAllRooms()
+    {
+        return Ok(_rooms);
     }
 
     [HttpPost]
     public async Task<ActionResult<Room>> CreateRoom(Room newRoom)
     {
         var created = newRoom with { Id = 42 };
+        _rooms.Add(created);
         return CreatedAtAction(nameof(GetRoomById), new { id = created.Id }, created);
     }
 }
